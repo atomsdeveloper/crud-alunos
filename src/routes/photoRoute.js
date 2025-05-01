b827870a5c895c1f99718513ed93bd41723e2ad7
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-01 20:59:37 -0300

    feat: created route photo

---
+++ src/routes/photoRoute.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+const router = new Router();
+
+// Multer
+import multer from "multer";
+
+// Controller
+import PhotoController from "../controllers/photoController.js";
+
+// Middleware
+import loginRequired from "../middlewares/loginRequired.js";
+
+// Multer Config
+import multerConfig from "../config/multer.js";
+
+// Middleware
+// Multer middleware to handle file uploads
+// The multerConfig is passed as an argument to the multer function
+// The single method is used to specify that we are expecting a single file upload
+const upload = multer(multerConfig).single("file");
+
+// Route
+router.post("/", loginRequired, upload, PhotoController.store);
+
+export default router;
+
+/*
+ index -> List all Students -> GET
+ show -> Show a Student  -> GET
+ store -> Create a Student -> POST
+ update -> Update a Student -> PUT
+ delete -> Delete a Student -> DELETE
+*/
