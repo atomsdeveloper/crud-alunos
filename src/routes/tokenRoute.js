72f8ab04b856c5a63a530bd2c3642f42e7c5502a
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-01 20:56:19 -0300

    feat: created route token

---
+++ src/routes/tokenRoute.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+const router = new Router();
+
+// Controller
+import tokenController from "../controllers/tokenController.js";
+
+router.get("/", tokenController.store);
+
+export default router;
+
+/*
+ index -> List all Tokens -> GET
+ show -> Show a Token  -> GET
+ store -> Create a Token -> POST
+ update -> Update a Token -> PUT
+ delete -> Delete a Token -> DELETE
+*/
