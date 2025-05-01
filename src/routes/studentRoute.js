6ae3c1e0110988d81659e32931a3cacbee79b2fd
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-01 20:55:40 -0300

    feat: created route student

---
+++ src/routes/studentRoute.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+const router = new Router();
+
+// Middleware
+import loginRequired from "../middlewares/loginRequired.js";
+
+// Controller
+import StudentController from "../controllers/studentController.js";
+
+// Routes
+
+// List all students
+router.get("/", StudentController.index);
+router.get("/:id", StudentController.show);
+router.post("/", loginRequired, StudentController.store);
+router.put("/:id", loginRequired, StudentController.update);
+router.delete("/:id", loginRequired, StudentController.delete);
+
+export default router;
+
+/*
+ index -> List all Students -> GET
+ show -> Show a Student  -> GET
+ store -> Create a Student -> POST
+ update -> Update a Student -> PUT
+ delete -> Delete a Student -> DELETE
+*/
