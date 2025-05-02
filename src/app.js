1e0b11292c26159a3723261da352dc520235a105
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-02 12:17:56 -0300

    feat: change local files and add cors and helmet

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import dotenv from "dotenv";
+// Load environment variables from .env file
+dotenv.config();
+
+import cors from "cors";
+import helmet from "helmet";
+
+import { resolve } from "path";
+import express from "express";
+
+// Importing the database connection
+import "./database/index.js";
+
+// Routes
+import homeRoute from "./routes/homeRoute.js";
+import userRoute from "./routes/userRoute.js";
+import tokenRoute from "./routes/tokenRoute.js";
+import studentRoute from "./routes/studentRoute.js";
+import photoRoute from "./routes/photoRoute.js";
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.app.use(cors());
+    this.app.use(helmet());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, "uploads")));
+  }
+
+  routes() {
+    this.app.use("/", homeRoute);
+    this.app.use("/users/", userRoute);
+    this.app.use("/token/", tokenRoute);
+    this.app.use("/students/", studentRoute);
+    this.app.use("/photo/", photoRoute);
+  }
+}
+
+// Exporting the instance of App
+// This allows us to use the same instance across the application
+export default new App().app;
