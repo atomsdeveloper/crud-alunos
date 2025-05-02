8102cc7157b4b323278f3aa52effa0d6b9a00c9f
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-01 22:31:20 -0300

    feat: edit database to check associete fields

--- src/database/index.js
+++ src/database/index.js
@@ -4,9 +4,10 @@ import configDatabase from "../config/database.js";
 // Importing the models
 import Aluno from "../models/Aluno.js";
 import User from "../models/User.js";
+import Photo from "../models/Photo.js";
 
 // This array contains all the models that will be initialized with the database connection
-const models = [Aluno, User];
+const models = [Aluno, User, Photo];
 
 // Create a new Sequelize instance with the database configuration
 // This instance will be used to connect to the database and perform operations
@@ -14,3 +15,7 @@ const connection = new Sequelize(configDatabase);
 
 // This function initializes the database connection and all the models
 models.forEach((model) => model.init(connection));
+// This function is used to synchronize the models with the database
+models.forEach(
+  (model) => model.associate && model.associate(connection.models)
+);
