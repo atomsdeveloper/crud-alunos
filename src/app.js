c1e748993f78569c6585896c7481cb344020197b
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-04 23:04:11 -0300

    feat: test set header to bug bad request

--- src/app.js
+++ src/app.js
@@ -52,6 +52,19 @@ class App {
   }
 
   middleware() {
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      res.header(
+        "Content-Security-Policy",
+        "default-src 'self'; connect-src 'self' http://localhost:5173 https://seu-dominio.vercel.app; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' https: data:; form-action 'self'; object-src 'none'; upgrade-insecure-requests"
+      );
+
+      next();
+    });
     this.app.use(cors(corsOptions));
     this.app.use(helmet(helmetOptions));
     this.app.use(express.json());
